451ce346be6348d313c9b1ae2d473c323b499111
Seminioni <lebatmann@gmail.com>
2018-01-17 11:57:16 +0300

    upd: reaupload fonts;

--- postcss.config.js
+++ postcss.config.js
@@ -11,34 +11,39 @@ let plugins = [
             normal: {
                300: {
                   url: {
-                     woff2: '../fonts/Lato/subset-Lato-Light.woff2',
-                     woff: '../fonts/Lato/subset-Lato-Light.woff'
+                     woff2: '../fonts/Lato/Lato-Light.woff2',
+                     woff: '../fonts/Lato/Lato-Light.woff',
+                     ttf: '../fonts/Lato/Lato-Light.ttf'
                   }
                },
                400: {
                   url: {
-                     woff2: '../fonts/Lato/subset-Lato-Regular.woff2',
-                     woff: '../fonts/Lato/subset-Lato-Regular.woff'
+                     woff2: '../fonts/Lato/Lato-Regular.woff2',
+                     woff: '../fonts/Lato/Lato-Regular.woff',
+                     ttf: '../fonts/Lato/Lato-Regular.ttf'
                   }
                },
                500: {
                  url: {
-                    woff2: '../fonts/Lato/subset-Lato-Medium.woff2',
-                    woff: '../fonts/Lato/subset-Lato-Medium.woff'
+                    woff2: '../fonts/Lato/Lato-Medium.woff2',
+                    woff: '../fonts/Lato/Lato-Medium.woff',
+                    ttf: '../fonts/Lato/Lato-Medium.ttf'
                  }
                }
             },
             italic: {
                300: {
                   url: {
-                    woff2: '../fonts/Lato/subset-Lato-LightItalic.woff2',
-                    woff: '../fonts/Lato/subset-Lato-LightItalic.woff',
+                    woff2: '../fonts/Lato/Lato-LightItalic.woff2',
+                    woff: '../fonts/Lato/Lato-LightItalic.woff',
+                    ttf: '../fonts/Lato/Lato-LightItalic.ttf'
                   }
                },
                500: {
                   url: {
-                    woff2: '../fonts/Lato/subset-Lato-MediumItalic.woff2',
-                    woff: '../fonts/Lato/subset-Lato-MediumItalic.woff',
+                    woff2: '../fonts/Lato/Lato-MediumItalic.woff2',
+                    woff: '../fonts/Lato/Lato-MediumItalic.woff',
+                    ttf: '../fonts/Lato/Lato-MediumItalic.ttf'
                   }
              }
             }
