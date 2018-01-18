9c2e217e170f64758a3d88f605c3b61665075937
Seminioni <lebatmann@gmail.com>
2018-01-18 21:14:52 +0300

    upd: finish coin-preview;

---
+++ src/javascripts/components/tooltip/index.js
@@ -0,0 +1,7 @@
+import tippy from 'tippy.js';
+
+tippy('.catalog-item__rare', {
+  placement: 'bottom',
+  duration: 300,
+  distance: 5
+})
---
+++ src/javascripts/controllers/stepper_controller.js
@@ -0,0 +1,19 @@
+// src/controllers/header_controller.js
+import { Controller } from "stimulus"
+
+export default class Stepper extends Controller {
+  onSideClick(e) {
+    const input = this.targets.find('stepperInput');
+    let inputVal = +input.value;
+
+    if (e.currentTarget.classList.contains('stepper__side--right')) {
+      inputVal += 1;
+    } else {
+      if (inputVal === 0) {
+        return
+      }
+      inputVal -= 1;
+    }
+    input.value = inputVal;
+  }
+}
