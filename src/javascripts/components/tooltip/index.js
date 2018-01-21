29102f34efb31a0844178f5581a28f690b56d9c9
seminioni <lebatmann@gmail.com>
2018-01-21 16:54:02 +0300

    add: tip destroing on desktop;

--- src/javascripts/components/tooltip/index.js
+++ src/javascripts/components/tooltip/index.js
@@ -1,7 +1,25 @@
 import tippy from 'tippy.js';
 
-tippy('.catalog-item__rare', {
+const tip = tippy('.catalog-item__rare', {
   placement: 'bottom',
   duration: 300,
   distance: 5
 })
+
+
+window.addEventListener('scroll', function () {
+  const poppers = document.querySelectorAll('.tippy-popper')
+
+  for (const popper of poppers) {
+    const tooltip = popper._reference._tippy
+
+    if (tooltip.state.visible) {
+      tooltip.popperInstance.disableEventListeners()
+      tooltip.hide()
+    }
+  }
+})
+
+if (window.matchMedia("screen and (min-width: 768px)").matches) {
+  tip.destroyAll()
+}
