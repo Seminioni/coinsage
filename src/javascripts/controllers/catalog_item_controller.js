0d2ac677c7027487d90bf3238bb88fc97c85e480
Seminioni <lebatmann@gmail.com>
2018-01-22 20:23:56 +0300

    upd: add catalogItemInner page and a lot of another stuff;

--- src/javascripts/controllers/catalog_item_controller.js
+++ src/javascripts/controllers/catalog_item_controller.js
@@ -1,37 +1,36 @@
 import { Controller } from "stimulus";
 import initSiema from "components/slider/index.js";
 
-export default class Favorite extends Controller {
+export default class CatalogItem extends Controller {
   initialize() {
+    const container = this.targets.find('slider');
 
-    const container = this.element.querySelector(
-      ".catalog-item__slider-container"
-    );
     const prev = this.targets.find('prev');
     const next = this.targets.find('next');
 
     const mySiema = initSiema({
-      selector: ".favorite .catalog__inner",
+      selector: container,
       duration: 500,
       easing: "ease-in-out",
-      perPage: {
-        768: 3,
-        1024: 4
-      },
+      perPage: 1,
       startIndex: 0,
       draggable: true,
-      multipleDrag: true,
+      multipleDrag: false,
       threshold: 20,
       loop: true,
-      onInit: function() {},
-      onChange: function() {}
+      onInit: function() {
+        this.addDots();
+        this.updateDots();
+      },
+      onChange: function() {
+        this.updateDots();
+      }
     });
+
+
 
     prev.addEventListener('click', () => mySiema.prev());
     next.addEventListener('click', () => mySiema.next());
 
-    if (window.matchMedia("screen and (max-width: 767px)").matches) {
-      mySiema.destroy(true)
-    }
   }
 }
