743e8085fdf292b9f31e4a4f59d497670f60f859
seminioni <lebatmann@gmail.com>
2018-02-04 21:21:38 +0300

    fix

--- src/javascripts/controllers/file_uploader_controller.js
+++ src/javascripts/controllers/file_uploader_controller.js
@@ -4,30 +4,44 @@ export default class FileUploader extends Controller {
   initialize() {
     const inputElement = this.targets.find("fileInput");
 
-    inputElement.addEventListener("change", (e) => {
-      this.target = e.target.files;
-      const fileNames = Array.from(e.target.files).map(obj => obj.name);
+    inputElement.addEventListener(
+      "change",
+      e => {
+        this.target = e.target.files;
+        const fileNames = Array.from(e.target.files).map(obj => obj.name);
 
-      this.renderFileNames(fileNames, e.target.files);
-    }, false);
+        this.renderFileNames(fileNames, e.target.files);
+      },
+      false
+    );
   }
 
   renderFileNames(names, kek) {
-    const uploadsList = this.targets.find('uploads-list');
+    const uploadsList = this.targets.find("uploads-list");
+
+    // function elementAlreadyExist(el) {
+    //   return Array.from(uploadsList.children).map(child => child.innerText).includes(el)
+    // }
+
+    uploadsList.innerHTML = '';
 
     function appendNames(array, i) {
       if (array[i] === undefined) {
         return;
       }
-      const fileName = document.createElement('p')
-      fileName.classList.add('file-uploader__uploads-item')
-      fileName.innerText = array[i]
+      // if (elementAlreadyExist(array[i])) {
+      //   return appendNames(array, ++i);
+      // }
+      const fileName = document.createElement("p");
+      fileName.classList.add("file-uploader__uploads-item");
+      fileName.innerText = array[i];
+      fileName.setAttribute("data-value", array[i]);
+
       uploadsList.appendChild(fileName);
 
       return appendNames(array, ++i);
     }
 
     appendNames(names, 0);
-
   }
 }
